f54276f2e8475fe207ad9da3edd5ad1f24001844
Eugene <evgeny.myasishchev@gmail.com>
2025-02-13 21:04:50 -0600

    Initial logger implementation.

--- packages/core/src/logger.ts
+++ packages/core/src/logger.ts
@@ -1,3 +1,6 @@
+import pino from 'pino';
+import type { Context, ContextValues } from './context.js';
+
 interface LogData {
   [key: string]: any // eslint-disable-line @typescript-eslint/no-explicit-any
 }
@@ -13,7 +16,15 @@ export const LogLevel = {
 
 export type LogLevel = typeof LogLevel[keyof typeof LogLevel]
 
-export type TestType = 'info' | 'error'
+type LevelMethods<
+  TLogLevels extends LogLevel,
+> = {
+  /**
+   * Write a log entry with a given level. Please use withData or withError to include
+   * them with the log being written.
+   */
+  [level in TLogLevels]: (msg: string) => void
+}
 
 /**
  * Unified logger interface. Key points:
@@ -21,10 +32,9 @@ export type TestType = 'info' | 'error'
  * 2. Standardizes the way arbitrary data is included with log messages
  * 3. Standardizes the way child loggers are created
  */
-export type Logger = {
+export type Logger = LevelMethods<LogLevel> & {
   /**
    * Creates a child logger of a given group name.
-   * Groups can be nested as many level deep as needed.
    * The way group is written to an underlying logger
    * is implementation specific.
    */
@@ -45,16 +55,6 @@ export type Logger = {
   */
   withError(error: unknown): Omit<Logger, 'withError' | 'withGroup'>
 
-  /*
-  * Write log entry with error level.
-  * In order to include error please use withError method
-  */
-  error(msg: string): void
-  warn(msg: string): void
-  info(msg: string): void
-  debug(msg: string): void
-  trace(msg: string): void
-
   /**
    * Clean way to write log with runtime defined level
    */
@@ -66,4 +66,62 @@ export type Logger = {
    * is a concern.
    */
   levelEnabled(level: LogLevel): boolean
+}
+
+export function createRootPinoLogger(opts: {
+  context: Context<ContextValues>
+
+  /**
+   * Pino logger instance that will be used to write logs and create child loggers.
+   * Please note that the level of the pino logger will be manipulated as needed so
+   * do not change it after the root logger initialization.
+   */
+  pinoLogger: pino.Logger
+}): Logger {
+  function createPinoLoggerAdapter(pinoLogger: pino.Logger): Logger {
+    const self: Logger = {
+      withGroup(name) {
+        return createPinoLoggerAdapter(pinoLogger.child({ group: name }));
+      },
+
+      withData(data) {
+        return createPinoLoggerAdapter(pinoLogger.child({ data }));
+      },
+
+      withError(error) {
+        return createPinoLoggerAdapter(pinoLogger.child({ err: error }));
+      },
+
+      write(level, msg) {
+        pinoLogger[level]({ context: opts.context.values }, msg);
+      },
+
+      error(msg) {
+        self.write('error', msg);
+      },
+
+      warn(msg) {
+        self.write('warn', msg);
+      },
+
+      info(msg) {
+        self.write('info', msg);
+      },
+
+      debug(msg) {
+        self.write('debug', msg);
+      },
+
+      trace(msg) {
+        self.write('trace', msg);
+      },
+
+      levelEnabled(level) {
+        return pinoLogger.isLevelEnabled(level);
+      },
+    };
+    return self;
+  }
+
+  return createPinoLoggerAdapter(opts.pinoLogger);
 }

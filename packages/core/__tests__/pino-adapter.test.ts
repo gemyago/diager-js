53d4df2a03a8ea9f78017ff077beb30587299f54
Eugene <evgeny.myasishchev@gmail.com>
2025-02-15 00:04:19 +0100

    Nest tests.

--- packages/core/__tests__/pino-adapter.test.ts
+++ packages/core/__tests__/pino-adapter.test.ts
@@ -17,30 +17,32 @@ describe('pino-adapter', () => {
     };
   };
 
-  it.each([
-    ['error' as const, 50],
-    ['warn' as const, 40],
-    ['info' as const, 30],
-    ['debug' as const, 20],
-    ['trace' as const, 10],
-  ] as const)('should write log messages with %s level', (levelName, levelNum) => {
-    const mockDestination = createMockDestination();
-    const diagCtxVals = { correlationId: randomUUID() };
-    const rootLogger = createRootPinoLogger({
-      context: createContext(diagCtxVals),
-      pinoLogger: pino.pino({
-        level: 'trace',
-      }, mockDestination),
-    });
+  describe('write methods', () => {
+    it.each([
+      ['error' as const, 50],
+      ['warn' as const, 40],
+      ['info' as const, 30],
+      ['debug' as const, 20],
+      ['trace' as const, 10],
+    ] as const)('should write log messages with %s level', (levelName, levelNum) => {
+      const mockDestination = createMockDestination();
+      const diagCtxVals = { correlationId: randomUUID() };
+      const rootLogger = createRootPinoLogger({
+        context: createContext(diagCtxVals),
+        pinoLogger: pino.pino({
+          level: 'trace',
+        }, mockDestination),
+      });
 
-    const wantMsg = faker.lorem.sentence();
-    rootLogger[levelName](wantMsg);
-    expect(mockDestination.written).toHaveLength(1);
-    const gotMessage = JSON.parse(mockDestination.written[0]);
-    expect(gotMessage).toMatchObject({
-      level: levelNum,
-      msg: wantMsg,
-      context: diagCtxVals,
+      const wantMsg = faker.lorem.sentence();
+      rootLogger[levelName](wantMsg);
+      expect(mockDestination.written).toHaveLength(1);
+      const gotMessage = JSON.parse(mockDestination.written[0]);
+      expect(gotMessage).toMatchObject({
+        level: levelNum,
+        msg: wantMsg,
+        context: diagCtxVals,
+      });
     });
   });
 });

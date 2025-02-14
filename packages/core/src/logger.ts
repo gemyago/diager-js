4f5f8f174ebd6b182ae63990772099e413d38c18
Eugene <evgeny.myasishchev@gmail.com>
2025-02-14 15:26:18 +0100

    Some basic example and more docs.

--- packages/core/src/logger.ts
+++ packages/core/src/logger.ts
@@ -1,4 +1,4 @@
-import pino from 'pino';
+import type pino from 'pino';
 import type { Context, ContextValues } from './context.js';
 
 interface LogData {
@@ -65,10 +65,22 @@ export type Logger = LevelMethods<LogLevel> & {
    * Can be used in performance critical scenarios where log data preparation
    * is a concern.
    */
-  levelEnabled(level: LogLevel): boolean
+  isLevelEnabled(level: LogLevel): boolean
 }
 
+/**
+ * Creates a root logger instance that is using underlying pino logger to write logs.
+ * Usually you would create a single root logger per application and then use create child loggers
+ * using withGroup method to create loggers for individual application components. Please prefer to
+ * avoid global loggers and instead initialize your components with explicit logger instances.
+ *
+ * Please feel free to configure the pino logger instance as needed. Please note that the level
+ * of the pino logger will be manipulated to allow context specific log level filtering.
+ */
 export function createRootPinoLogger(opts: {
+  /**
+   * Instance of the diagnostic context associated with this logger.
+   */
   context: Context<ContextValues>
 
   /**
@@ -79,6 +91,8 @@ export function createRootPinoLogger(opts: {
   pinoLogger: pino.Logger
 }): Logger {
   function createPinoLoggerAdapter(pinoLogger: pino.Logger): Logger {
+    const initialLevel = pinoLogger.levelVal;
+    pinoLogger.level = 'trace'; // eslint-disable-line no-param-reassign -- we are filtering logs on our own so have to set this to min value
     const self: Logger = {
       withGroup(name) {
         return createPinoLoggerAdapter(pinoLogger.child({ group: name }));
@@ -93,7 +107,9 @@ export function createRootPinoLogger(opts: {
       },
 
       write(level, msg) {
-        pinoLogger[level]({ context: opts.context.values }, msg);
+        if (self.isLevelEnabled(level)) {
+          pinoLogger[level]({ context: opts.context.values }, msg);
+        }
       },
 
       error(msg) {
@@ -116,8 +132,22 @@ export function createRootPinoLogger(opts: {
         self.write('trace', msg);
       },
 
-      levelEnabled(level) {
-        return pinoLogger.isLevelEnabled(level);
+      isLevelEnabled(level) {
+        const levelVal = pinoLogger.levels.values[level];
+
+        // If we don't know how to check it, we just delegate to pino
+        if (!levelVal) {
+          return pinoLogger.isLevelEnabled(level);
+        }
+
+        // We either use context "overridden" value or the initial value
+        const desiredMinLevelValue = opts.context.values.minLogLevel
+          ? pinoLogger.levels.values[opts.context.values.minLogLevel]
+          : initialLevel;
+        if (levelVal < desiredMinLevelValue) {
+          return false;
+        }
+        return true;
       },
     };
     return self;

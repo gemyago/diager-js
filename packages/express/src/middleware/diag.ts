31bf70e035361dbaf213fd536d54aaa3ffe7020c
Eugene Myasyshchev <eugenemyasyshchev@gmail.com>
2025-02-15 23:22:30 +0100

    Some more docs.

--- packages/express/src/middleware/diag.ts
+++ packages/express/src/middleware/diag.ts
@@ -2,7 +2,8 @@ import { Context, ContextValues } from '@diager-js/core';
 import type { RequestHandler } from 'express';
 
 /**
- * Initializes diag context with values from the request.
+ * Initializes diag context with values from the request. This middleware
+ * should usually be very first in the middleware chain.
  */
 export function createDiagMiddleware<
   TContextValues extends ContextValues = ContextValues
@@ -18,14 +19,15 @@ export function createDiagMiddleware<
    * header name and value is a context value. The middleware
    * will assign corresponding context values from the headers.
    *
-   * If the correlationId is not initialized after mapping headers, the middleware will generate
-   * a new correlationId.
+   * If the correlationId is not initialized after mapping headers,
+   * the middleware will attempt X-Correlation-ID and X-Request-ID headers.
+   * It will will generate a new correlationId as a last resort.
    *
    * If minLogLevel is not initialized after mapping headers, the values
    * will be attempted to be taken from X-Min-Log-Level header. Invalid
    * values will be discarded.
    */
-  contextHeaders: Record<string, keyof TContextValues>;
+  contextHeaders?: Record<string, keyof TContextValues>;
 
   /**
    * Defines mapping between request parameters and context values where key is the

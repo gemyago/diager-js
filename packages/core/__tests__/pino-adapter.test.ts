0d5804d42edad1ca90599dfe7b296cd82b4fea68
Eugene <evgeny.myasishchev@gmail.com>
2025-02-15 16:19:43 +0100

    Pending stuff.

--- packages/core/__tests__/pino-adapter.test.ts
+++ packages/core/__tests__/pino-adapter.test.ts
@@ -6,43 +6,90 @@ import { createRootPinoLogger } from '../src/pino-adapter.js';
 
 describe('pino-adapter', () => {
   const createMockDestination = () => {
-    const written: string[] = [];
+    const written: object[] = [];
     return {
       get written() {
         return written;
       },
       write(data: string) {
-        written.push(data);
+        written.push(JSON.parse(data));
       },
     };
   };
 
   describe('write methods', () => {
-    it.each([
+    const knownLevels = [
       ['error' as const, 50],
       ['warn' as const, 40],
       ['info' as const, 30],
       ['debug' as const, 20],
       ['trace' as const, 10],
-    ] as const)('should write log messages with %s level', (levelName, levelNum) => {
-      const mockDestination = createMockDestination();
+    ] as const;
+
+    function createMockDeps() {
       const diagCtxVals = { correlationId: randomUUID() };
+      const context = createContext(diagCtxVals);
+      const mockDestination = createMockDestination();
+      return {
+        mockDestination,
+        diagCtxVals,
+        context,
+      };
+    }
+
+    it.each(knownLevels)('should write log messages with %s level', (levelName, levelNum) => {
+      const mockDeps = createMockDeps();
       const rootLogger = createRootPinoLogger({
-        context: createContext(diagCtxVals),
-        pinoLogger: pino.pino({
-          level: 'trace',
-        }, mockDestination),
+        context: mockDeps.context,
+        pinoLogger: pino.pino({ level: 'trace' }, mockDeps.mockDestination),
       });
 
       const wantMsg = faker.lorem.sentence();
       rootLogger[levelName](wantMsg);
-      expect(mockDestination.written).toHaveLength(1);
-      const gotMessage = JSON.parse(mockDestination.written[0]);
+      expect(mockDeps.mockDestination.written).toHaveLength(1);
+      const gotMessage = mockDeps.mockDestination.written[0];
       expect(gotMessage).toMatchObject({
         level: levelNum,
         msg: wantMsg,
-        context: diagCtxVals,
+        context: mockDeps.diagCtxVals,
       });
+    });
+
+    describe('generic write', () => {
+      it('should write log messages with runtime defined level', () => {
+        const mockDeps = createMockDeps();
+        const rootLogger = createRootPinoLogger({
+          context: mockDeps.context,
+          pinoLogger: pino.pino({ level: 'trace' }, mockDeps.mockDestination),
+        });
+
+        const wantLevel = faker.helpers.arrayElement(knownLevels);
+        const wantMsg = faker.lorem.sentence();
+        rootLogger.write(wantLevel[0], wantMsg);
+        expect(mockDeps.mockDestination.written).toHaveLength(1);
+        const gotMessage = mockDeps.mockDestination.written[0];
+        expect(gotMessage).toMatchObject({
+          level: wantLevel[1],
+          msg: wantMsg,
+          context: mockDeps.diagCtxVals,
+        });
+      });
+
+      it('should not write log messages with level below minLogLevel', () => {
+        const mockDeps = createMockDeps();
+        const rootLogger = createRootPinoLogger({
+          context: mockDeps.context,
+          pinoLogger: pino.pino({ level: 'info' }, mockDeps.mockDestination),
+        });
+
+        const wantMsg = faker.lorem.sentence();
+        rootLogger.write('debug', wantMsg);
+        expect(mockDeps.mockDestination.written).toHaveLength(0);
+      });
+    });
+
+    describe('isLevelEnabled', () => {
+
     });
   });
 });

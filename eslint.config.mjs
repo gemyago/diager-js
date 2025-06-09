340bbdec65ba33c4de2dd1e760e1cba4be5a539d
Eugene Myasyshchev <eugenemyasyshchev@gmail.com>
2025-06-09 08:37:20 +0200

    Enable prettier.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,40 @@
-import js from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import importPlugin from 'eslint-plugin-import';
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import importPlugin from "eslint-plugin-import";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default tseslint.config([
   js.configs.recommended,
   tseslint.configs.recommended,
+  eslintConfigPrettier,
+  eslintPluginPrettierRecommended,
 
+  // Prettier
   {
-    files: ['**/*.{js,ts,tsx}'],
-    extends: [importPlugin.flatConfigs.recommended, importPlugin.flatConfigs.typescript],
+    files: ["**/*.{js,ts,tsx}"],
+
+    rules: {
+      "prettier/prettier": ["error", { singleQuote: true }],
+    },
+  },
+
+  // Import
+  {
+    files: ["**/*.{js,ts,tsx}"],
+    extends: [
+      importPlugin.flatConfigs.recommended,
+      importPlugin.flatConfigs.typescript,
+    ],
     settings: {
-      'import/extensions': ['.js', '.ts', '.tsx'],
-      'import/resolver': {
+      "import/extensions": [".js", ".ts", ".tsx"],
+      "import/resolver": {
         typescript: {
           alwaysTryTypes: true,
-          project: './tsconfig.json',
+          project: "./tsconfig.json",
         },
         node: {
-          extensions: ['.js', '.jsx'],
+          extensions: [".js", ".jsx"],
         },
       },
     },
@@ -25,18 +42,14 @@ export default tseslint.config([
 
   // Ignores
   {
-    ignores: [
-      '**/node_modules/**',
-      '**/dist/**',
-      '**/jest.config.cjs',
-    ],
+    ignores: ["**/node_modules/**", "**/dist/**", "**/jest.config.cjs"],
   },
-])
+]);
 
 // [
 //   // Apply to all files
 //   js.configs.recommended,
-  
+
 //   // TypeScript files
 //   {
 //     files: ['**/*.ts', '**/*.tsx'],
@@ -54,18 +67,18 @@ export default tseslint.config([
 //       // ESLint rules
 //       'no-console': 'error',
 //       'no-restricted-syntax': 'off',
-      
+
 //       // Disable base rules that are covered by TypeScript
 //       'no-unused-vars': 'off',
 //       'no-shadow': 'off',
-      
+
 //       // TypeScript rules
 //       '@typescript-eslint/no-unused-vars': 'error',
 //       '@typescript-eslint/no-explicit-any': 'error',
 //       '@typescript-eslint/no-shadow': 'error',
 //     },
 //   },
-  
+
 //   // JavaScript files
 //   {
 //     files: ['**/*.js', '**/*.cjs', '**/*.mjs'],
@@ -74,7 +87,7 @@ export default tseslint.config([
 //       'no-restricted-syntax': 'off',
 //     },
 //   },
-  
+
 //   // Ignores
 //   {
 //     ignores: [
